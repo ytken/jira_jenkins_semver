127ad7078d91c403b0a0f07cacf357c551e27dad
ytken <1909dande.lion@gmail.com>
2023-04-19 15:01:24 +0300

    feat(create_issue): put Jira init to def

--- create_issue.py
+++ create_issue.py
@@ -11,7 +11,7 @@ def build_custom_issue(summary,assignee_name, description_path,
     issue_fields = {
         "project"     : {"key": proj_key},
         "issuetype"   : {"name": issue_type},   # Bug Task Story Epic
-        "summary"     : summary,
+        "summary"     : "[REGRESS][{}] Тесты падают на регрессе".format(summary),
         "reporter"    : {"name":reporter_name},
         "description" : open(description_path, "r").read(),
         "priority"    : {"name": priority}, # Highest High Medium Low Lowest Blocker
@@ -19,6 +19,19 @@ def build_custom_issue(summary,assignee_name, description_path,
         "assignee"    : {"name": assignee_name}
     }
     return issue_fields
+
+def get_jira():
+    jira_options = {'server': 'https://j-ymp.yadro.com'}
+    jira = JIRA(options=jira_options, basic_auth=("a.ovchinnikova", "IreN951a"))
+    return jira
+
+def check_for_issue(summary):
+    jira = get_jira()
+    issues = jira.search_issues("project = JenkinsAutoBug AND summary ~ \"{}\"".format(summary))
+    if issues:
+        return sorted(issues, key=lambda x: x.id, reverse=True)[0]
+    else:
+        return None
 
 def create_jira_issue(issue_fields):
     jira_options = {'server': 'http://172.18.0.1:7070'}
