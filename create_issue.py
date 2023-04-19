5349fa16619a0ff76d528a81f44a582112e3444c
ytken <1909dande.lion@gmail.com>
2023-04-19 14:54:50 +0300

    Some message

--- create_issue.py
+++ create_issue.py
@@ -1,7 +1,7 @@
 import sys
 from jira import JIRA
 
-def build_custom_issue(summary,assignee_name, description_path, 
+def build_custom_issue(summary,assignee_name, description_path,
                 proj_key="JAB",
                 issue_type="Bug",
                 reporter_name="a.ovchinnikova",
@@ -21,8 +21,11 @@ def build_custom_issue(summary,assignee_name, description_path,
     return issue_fields
 
 def create_jira_issue(issue_fields):
-    jira_options = {'server': 'https://j-ymp.yadro.com'}
-    jira = JIRA(options=jira_options, basic_auth=("a.ovchinnikova", "IreN951a"))
+    jira_options = {'server': 'http://172.18.0.1:7070'}
+    jira = JIRA(options=jira_options, basic_auth=("a.ovchinnikova", "Rozalija"))
+#    key_cert_data = open("cert_data.txt","r").read()
+#    oauth_dict = {'access_token': '5QoAgLbgjqhnIF1m6V7zhSLOre2KCDjy', 'access_token_secret': 'iHFMz1Hehds4ki7QeWWCpft7YwPCJtpA', 'consumer_key': 'jira-oauth-consumer', 'key_cert': key_cert_data}
+#    jira = JIRA(options=jira_options, oauth=oauth_dict)
     jira.create_issue(issue_fields)
 
 create_jira_issue(build_custom_issue(*sys.argv[1:]))
